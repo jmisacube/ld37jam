8029ac567288acd8ffb5067a03f9c0cf0946d609
Bitzawolf <bitzah@gmail.com>
2016-12-11 18:49:48 -0800

    Added bubble jump UI

---
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/UICircle.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+[RequireComponent(typeof(Image))]
+public class UICircle : MonoBehaviour
+{
+    [SerializeField] private Sprite ChargedSprite, SpentSprite;
+    private bool isCharged;
+    private Image uiImage;
+
+    void Start ()
+    {
+        isCharged = true;
+        uiImage = GetComponent<Image>();
+	}
+
+    public void setCharged(bool b)
+    {
+        if (isCharged == b)
+            return;
+
+        if (isCharged)
+            uiImage.sprite = SpentSprite;
+        else
+            uiImage.sprite = ChargedSprite;
+
+        isCharged = b;
+    }
+}
