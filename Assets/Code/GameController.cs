7c45973f558aa32576e06e074470d88d1e038461
Bitzawolf <bitzah@gmail.com>
2016-12-11 20:23:52 -0800

    Added End-Goal Ball

---
+++ Assets/Code/GameController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
---
+++ Assets/Code/Win.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Win : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("Quit. ACTIVATE");
+            Application.Quit();
+        }
+    }
+}
